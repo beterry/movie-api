370b2f2acfd87c7a24be20921925a7e65f4662e4
Ben Terry <beterry12@gmail.com>
2020-10-07 17:10:08 -0400

    render movies from API

--- app.js
+++ app.js
@@ -1,28 +1,64 @@
 const form = document.querySelector('#search-form');
-const results = document.querySelector('#results')
+const results = document.querySelector('#results');
+const resultHeader = document.querySelector('#result-header');
 
-let movieList = ''
+const key = "32e2293c913c59efce4695064290d9fb";
+const imageURL = "https://image.tmdb.org/t/p/w500";
+
+let movieList = '';
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(form.search.value);
-})
+    searchFromUser = form.search.value;
+    
 
-for(let i = 0; i < 4; i++){
-    movieList += `
-        <li class="movie">
-            <img src="https://image.tmdb.org/t/p/w500/rhr4y79GpxQF9IsfJItRXVaoGs4.jpg" alt="" />
-            <h3>Jurassic World</h3>
-            <div class="movie-detail">
-                <h4>Year</h4>
-                <p>2018</p>
-            </div>
-            <div class="movie-detail">
-                <h4>Director</h4>
-                <p>Steven Speilburg</p>
-            </div>
-        </li>
-    `;
+    //format the string to use in URL
+    searchFormatted = searchFromUser.replace(/\s+/g, '+').toLowerCase();
+
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${searchFormatted}`)
+    .then((res) => {
+        if(res.data.results.length > 0){
+            resultHeader.textContent = `Top Results for '${searchFromUser}'`;
+            renderMovies(res.data.results);
+        } else {
+            resultHeader.textContent = `No Results for '${searchFromUser}'`;
+            results.innerHTML = '';
+        }
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+});
+
+function renderMovies(movies){
+    movieList = '';
+
+    for(let i = 0; i < 4 && movies[i] != undefined; i++){
+        const movie = movies[i];
+
+        const title = movie.title;
+        const releaseDate = new Date(movie.release_date)
+        const releaseYear =  releaseDate.getFullYear();
+        const poster = movie.poster_path;
+        let posterURL = '';
+
+        if(poster){
+            posterURL = `${imageURL}${poster}`;
+        } else {
+            posterURL = "https://critics.io/img/movies/poster-placeholder.png";
+        }
+
+        movieList += `
+            <li class="movie">
+                <img src=${posterURL} alt="Movie poster" />
+                <div class="movie-details">
+                    <p>${releaseYear}</p>
+                    <h3>${title}</h3>
+                </div>
+            </li>
+        `;
+    }
+
+    results.innerHTML = movieList;
+    form.search.value = '';
 }
-
-results.innerHTML = movieList;
