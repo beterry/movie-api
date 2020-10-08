e370b69b3d3bad0a1654cef12481acdbeb18d2de
Ben Terry <beterry12@gmail.com>
2020-10-08 16:28:28 -0400

    code comments

--- app.js
+++ app.js
@@ -11,16 +11,18 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     searchFromUser = form.search.value;
     
-
     //format the string to use in URL
     searchFormatted = searchFromUser.replace(/\s+/g, '+').toLowerCase();
 
+    //request data from API
     axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${searchFormatted}`)
     .then((res) => {
         if(res.data.results.length > 0){
+            //there are title matches
             resultHeader.textContent = `Top Results for '${searchFromUser}'`;
             renderMovies(res.data.results);
         } else {
+            //there are no title matches
             resultHeader.textContent = `No Results for '${searchFromUser}'`;
             results.innerHTML = '';
         }
@@ -33,21 +35,25 @@ form.addEventListener('submit', (e) => {
 function renderMovies(movies){
     movieList = '';
 
+    //loops through up to 4 items
     for(let i = 0; i < 4 && movies[i] != undefined; i++){
         const movie = movies[i];
 
+        //variables for data
         const title = movie.title;
         const releaseDate = new Date(movie.release_date)
         const releaseYear =  releaseDate.getFullYear();
         const poster = movie.poster_path;
-        let posterURL = '';
 
+        //create URL for poster image or use placeholder
+        let posterURL = '';
         if(poster){
             posterURL = `${imageURL}${poster}`;
         } else {
             posterURL = "https://critics.io/img/movies/poster-placeholder.png";
         }
 
+        //movie component
         movieList += `
             <li class="movie">
                 <img src=${posterURL} alt="Movie poster" />
@@ -59,6 +65,8 @@ function renderMovies(movies){
         `;
     }
 
+    //inject components into DOM
     results.innerHTML = movieList;
+    //reset form
     form.search.value = '';
 }
